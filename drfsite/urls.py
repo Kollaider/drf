c95e9f5a1b495aa94f43312dd7cc2ab184c7588f
kolaider <Strommer87>
2022-11-28 15:28:48 +0300

    8. ModelViewSet

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -20,7 +20,6 @@ from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/womenlist', WomenViewSet.as_view({'get': 'list'})),
+    path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,20 +8,20 @@ from women.models import Women
 from women.serializers import WomenSerializer
 
 
-# class WomenAPIView(generics.ListAPIView):
+class WomenViewSet(viewsets.ModelViewSet):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+# class WomenAPIList(generics.ListCreateAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
-
-class WomenAPIList(generics.ListCreateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-class WomenAPIUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+#
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
