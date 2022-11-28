4b9b6a4079d22eaff81ac05c470a294ca1b3c133
kolaider <Strommer87>
2022-11-28 18:51:40 +0300

    9. custom Router

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -20,10 +20,34 @@ from women.views import *
 from rest_framework import routers
 
 
+class MyCustomReadOnlyRouter(routers.SimpleRouter):
+    """
+    A router for read-only APIs, which doesn't use trailing slashes.
+    """
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        ),
+    ]
+
+
 # router = routers.SimpleRouter()
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
+router = MyCustomReadOnlyRouter()
 router.register(r'women', WomenViewSet, basename='women')  # basename is required if not queryset in Vieset
 print(router.urls)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
