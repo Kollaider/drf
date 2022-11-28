9462764810090481fba704053b21fa0e679d16f6
kolaider <Strommer87>
2022-11-28 19:09:14 +0300

    10. 1. permission IsAuthenticatedOrReadOnly; 2. serializers.HiddenField

--- women/serializers.py
+++ women/serializers.py
@@ -8,6 +8,8 @@ from women.models import Women
 
 
 class WomenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Women
         # fields = ('title', 'content', 'category')
--- women/views.py
+++ women/views.py
@@ -2,33 +2,18 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from women.models import Women, Category
 from women.serializers import WomenSerializer
 
-#
-# # class WomenViewSet(viewsets.ReadOnlyModelViewSet):
-# class WomenViewSet(viewsets.ModelViewSet):
-#     # queryset = Women.objects.all() # basename
-#     serializer_class = WomenSerializer
-#
-#     def get_queryset(self):
-#         pk = self.kwargs.get('pk')
-#         if not pk:
-#             return Women.objects.all()[:3]
-#
-#         return Women.objects.filter(pk=pk)
-#
-#     @action(methods=['get'], detail=True)
-#     def category(self, request, pk=None):
-#         cat = Category.objects.get(pk=pk)
-#         return Response({'cats': cat.name})
 
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
